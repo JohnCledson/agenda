623750fe448e4ddd6f0baabc26e1a5011ceddbc1
John Cledson <cledsonjohn123@gmail.com>
2019-07-11 22:09:29 -0300

    Criando pagina de agenda mostrando os eventos

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+def local(request, titulo_evento):
+    return HttpResponse(Evento.objects.get(titulo=titulo_evento))
+
+def lista_eventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'eventos':evento}
+    return render(request, 'agenda.html', dados)
